a8702aca8dece8c3a34f3874f507e51f4d83df6f
Fyphen1223 <fyphensub@gmail.com>
2023-06-10 08:39:39 +0900

    Server

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-class archain {
-    constructor() {
+const express = require("express");
 
+export class archain {
+    constructor(port, settings) {
+        this.port = port;
+        this.settings = settings;
     }
-
-
-
+    start() {
+        const server = express();
+        if(typeof(this.port) !== Number) { 
+            throw new AssignError("Unauthorized port number. Assign Int.");
+            return;
+        }
+        server.listen(this.port);
+        server.get("/", async function(req, res) {
+            res.send("The server is wake up");
+        });
+        server.get("/search", async function(req, res) {
+            
+        });
+    }
 }
---
+++ index.ts
@@ -0,0 +1,10 @@
+const express = require("express");
+const server = express();
+
+export class archain {
+    constructor(num:port, json:settings) {
+        this.port = port;
+        this.settings = settings;
+    }
+
+}
