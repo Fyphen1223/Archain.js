78fd732c4a81f4c97f56d9dd0ca00fdbdc73ef64
Fyphen1223 <fyphensub@gmail.com>
2023-06-10 14:14:53 +0900

    @Fyphen1223 Updated

---
+++ test.ts
@@ -0,0 +1,10 @@
+export class archainTS {
+    constructor(port, settings) {
+        this.port = port;
+        this.settings = settings;
+    }
+    hello() {
+        let text:string = "Hello World"
+        console.log(text);
+    }
+}
